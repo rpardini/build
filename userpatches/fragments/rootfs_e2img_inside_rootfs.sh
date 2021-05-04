2e4b2a71a6e0e0b6aa59d91ba51dc98f77bf2099
Ricardo Pardini <ricardo@pardini.net>
2021-05-04 11:26:23 +0200

    fix rootfs-in-rootfs size multiplier, make configurable
    Signed-off-by: Ricardo Pardini <ricardo@pardini.net>

--- userpatches/fragments/rootfs_e2img_inside_rootfs.sh
+++ userpatches/fragments/rootfs_e2img_inside_rootfs.sh
@@ -1,14 +1,19 @@
+## Configuration
+
+export ROOTFS_IN_ROOTFS_SIZE_PERCENT=240 # 2.4 times the size of a simple rootfs seems to hit the target. squeeze to your liking.
+
 config_prepare_image_size__rootfs_predict_image_size() {
 	# For this to work we need a very big image size. We''ll fill it with itself, so get the size and double it at least.
 	# Note: the file is sparse, meaning that is shows larger than it actually is (filled with zeroes).
-	# For now well use triple the size.
-	export FIXED_IMAGE_SIZE=$(("${rootfs_size}" * 3))
-	display_alert "Determined FIXED_IMAGE_SIZE to be" "${FIXED_IMAGE_SIZE}" "info"
+	# ROOTFS_IN_ROOTFS_SIZE_PERCENT is the magic number, determined empirically.
+	export FIXED_IMAGE_SIZE=$((rootfs_size * ROOTFS_IN_ROOTFS_SIZE_PERCENT / 100))
+	display_alert "Predicted size of rootfs+rootfs e2img dump to be" "${FIXED_IMAGE_SIZE}Mb" "info"
 }
 
 config_pre_umount_final_image__prepare_rootfs_inside_rootfs() {
 	# For sure there is an Armbian env (LOOP_X?) but I didnt figure it out a the time.
-	export ROOT_LOOP_DEV_PART=$(mount | grep "${MOUNT}" | grep ext4 | cut -d " " -f 1)
+	export ROOT_LOOP_DEV_PART
+	ROOT_LOOP_DEV_PART=$(mount | grep "${MOUNT}" | grep ext4 | cut -d " " -f 1)
 }
 
 # No real need to compress. The file created is sparse, and ext4 works ok with those.
@@ -29,6 +34,7 @@ config_post_umount_final_image__rootfs_e2img_inside_rootfs() {
 	e2image -rap "${ROOT_LOOP_DEV_PART}" "${MOUNT}/../rootfs.ext4.e2img" >/dev/null 2>&1
 	sync
 
+	local apparent_size actual_size
 	apparent_size=$(du -h --apparent-size "${MOUNT}/../rootfs.ext4.e2img" | tr -s "\t" "|" | cut -d "|" -f 1)
 	actual_size=$(du -h "${MOUNT}/../rootfs.ext4.e2img" | tr -s "\t" "|" | cut -d "|" -f 1)
 
@@ -42,6 +48,7 @@ config_post_umount_final_image__rootfs_e2img_inside_rootfs() {
 	cp "${MOUNT}/../rootfs.ext4.e2img" "${MOUNT}/root/rootfs.ext4.e2img" || {
 		echo "" # break a line so error is clearly visible
 		display_alert "e2image sparse copy failed" "sizes: apparent: ${apparent_size} actual: ${actual_size} imgsize: ${FIXED_IMAGE_SIZE}" "err"
+		display_alert "e2image sparse copy failed" "please increase ROOTFS_IN_ROOTFS_SIZE_PERCENT (currently ${ROOTFS_IN_ROOTFS_SIZE_PERCENT})" "err"
 	}
 	sync
 
