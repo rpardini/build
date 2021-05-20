05197b12c11ec8871616c98b90d10b8807e0e04a
Ricardo Pardini <ricardo@pardini.net>
2021-05-20 23:16:16 +0200

    cloud-init: multiple fixes - extract heredocs in .yaml for better formatting - bump c-i log level - add ConfigDisk datasource - write network-config.sample if dhcp and eth0 (avoid bug with netplan) - add -cloud suffix to ${version}
    Signed-off-by: Ricardo Pardini <ricardo@pardini.net>

--- userpatches/fragments/cloud-init/cloud-init.sh
+++ userpatches/fragments/cloud-init/cloud-init.sh
@@ -14,18 +14,11 @@ export CLOUD_INIT_CONFIG_LOCATION="/boot" # where on the sdcard c-i will look fo
 # Change to eth0-dhcp-wait to use https:// includes in user-data, or to something else for non-ethernet devices
 export CLOUD_INIT_NET_CONFIG_FILE="eth0-dhcp"
 
-# Extra kernel cmdline parameters to add to SD and to eMMC. Used by cloud-init to determine type of boot.
-# Notice: in this fragment it is implemented as hacks to armbianEnv.txt;
-#         if you're *also* using the jumpstart module, it will use the same vars via extlinux.conf boot, for convenience.
-export KERNEL_EXTRA_CMDLINE_SD="oldskool.boottype=sd"     # Extra kernel cmdline to add to jumpstart/sd boot, before flashing to eMMC.
-export KERNEL_EXTRA_CMDLINE_EMMC="oldskool.boottype=emmc" # Extra kernel cmdline to add to emmc boot, after jumpstart.
-
-# This runs after install_common() and chroot_installpackages_local()
-# Inside customize_image(), before running the actual custom script.
-# We dont use the custom script so actual image customization is done here
-# not clear what happens after this? see below
 image_tweaks_pre_customize__cloud_init() {
-	echo -e "# configure cloud-init for NoCloud\ndatasource_list: [ NoCloud, None ]\ndatasource:\n  NoCloud: \n    dsmode: local\n    seedfrom: /boot/" >>"${SDCARD}"/etc/cloud/cloud.cfg.d/99-armbian.cfg
+	cp "${FRAGMENT_DIR}"/config/cloud-cfg.yaml "${SDCARD}"/etc/cloud/cloud.cfg.d/99-armbian-boot.cfg
+
+	# Learn how Ubuntu does things by reading lxd docs...:
+	# https://lxd.readthedocs.io/en/latest/cloud-init
 
 	cp "${FRAGMENT_DIR}"/config/meta-data.yaml "${SDCARD}${CLOUD_INIT_CONFIG_LOCATION}"/meta-data
 	echo -e "\n\ninstance-id: armbian-${BOARD}" >>"${SDCARD}${CLOUD_INIT_CONFIG_LOCATION}"/meta-data
@@ -35,9 +28,16 @@ image_tweaks_pre_customize__cloud_init() {
 	# This module has hook points, just like the regular Armbian build system. So fragments can influence other fragments. Neat?
 	# In this case, fragments compete to modify CLOUD_INIT_NET_CONFIG_FILE, so the ordering of the hooks is extremely important.
 	[[ $(type -t cloud_init_determine_network_config_template) == function ]] && cloud_init_determine_network_config_template
-	display_alert "Using network-config (final)" "network-configs/${CLOUD_INIT_NET_CONFIG_FILE}.yaml" "info"
 
-	cp "${FRAGMENT_DIR}"/config/network-configs/${CLOUD_INIT_NET_CONFIG_FILE}.yaml "${SDCARD}${CLOUD_INIT_CONFIG_LOCATION}"/network-config
+	# Hack, some wierd bug with c-i causes "match:" devices to not be brought up.
+	# For now just don't write a default network-config, c-i's default/fallback detection will dhcp it anyway (and that works).
+	if [[ ${CLOUD_INIT_NET_CONFIG_FILE} == *"eth0-dhcp"* ]]; then
+		display_alert "dhcp-variant (${CLOUD_INIT_NET_CONFIG_FILE})" "written as ${CLOUD_INIT_CONFIG_LOCATION}/network-config.sample" ""
+		cp "${FRAGMENT_DIR}"/config/network-configs/${CLOUD_INIT_NET_CONFIG_FILE}.yaml "${SDCARD}${CLOUD_INIT_CONFIG_LOCATION}"/network-config.sample
+	else
+		display_alert "Using network-config" "network-configs/${CLOUD_INIT_NET_CONFIG_FILE}.yaml" "info"
+		cp "${FRAGMENT_DIR}"/config/network-configs/${CLOUD_INIT_NET_CONFIG_FILE}.yaml "${SDCARD}${CLOUD_INIT_CONFIG_LOCATION}"/network-config
+	fi
 
 	# overwrite default (user-oriented) user-data with direct #include via CLOUD_INIT_USER_DATA_URL (automation oriented)
 	if [[ "a${CLOUD_INIT_USER_DATA_URL}" != "afiles" ]]; then
@@ -45,13 +45,19 @@ image_tweaks_pre_customize__cloud_init() {
 		echo -e "#include\n${CLOUD_INIT_USER_DATA_URL}" >"${SDCARD}${CLOUD_INIT_CONFIG_LOCATION}"/user-data
 	fi
 
-	# hack, the fact is I really can't understand cloud-init "DataSourceNoCloud" and its relation to network-config.
+	# Configure logging for cloud-init. INFO is too little and DEBUG too much (as always)
+	cp "${FRAGMENT_DIR}"/config/debug_logging.yaml "${SDCARD}"/etc/cloud/cloud.cfg.d/05_logging.cfg
+
 	# seed the /var/lib/cloud/seed/nocloud directory with symlinks to ${CLOUD_INIT_CONFIG_LOCATION}/*-data|config
+	# symlinks always there, be dangling or not.
 	local seed_dir="${SDCARD}"/var/lib/cloud/seed/nocloud
 	mkdir -p "${seed_dir}"
+	ln -s "${CLOUD_INIT_CONFIG_LOCATION}/network-config" "${seed_dir}"/network-config
 	ln -s "${CLOUD_INIT_CONFIG_LOCATION}/user-data" "${seed_dir}"/user-data
 	ln -s "${CLOUD_INIT_CONFIG_LOCATION}/meta-data" "${seed_dir}"/meta-data
-	ln -s "${CLOUD_INIT_CONFIG_LOCATION}/network-config" "${seed_dir}"/network-config
+
+	# remove any networkd config leftover from armbian build
+	rm -f "${SDCARD}"/etc/systemd/network/*.network || true
 
 	# cleanup -- cloud-init makes some Armbian stuff actually get in the way
 	[[ -f "${SDCARD}/boot/armbian_first_run.txt.template" ]] && rm -f "${SDCARD}/boot/armbian_first_run.txt.template"
@@ -100,45 +106,6 @@ user_config_post_aggregate_packages__900_confirm_cloudinit_packages() {
 
 }
 
-# almost the last thing before copying to SD.
-# this is called by post_debootstrap_tweaks() after ${SDCARD} is chroot_unmounted
-# -> which is called by  debootstrap_ng() before prepare_partitions()/create_image()
-# -> tmpfs is still going at this point.
-config_post_debootstrap_tweaks__hack_armbianEnv_ci_args() {
-	# This really is used for jumpstart and emmc, no way to actually discern yet.
-	# shellcheck disable=SC2129
-	echo "extraboardargs=${KERNEL_EXTRA_CMDLINE_SD}" >>"${SDCARD}"/boot/armbianEnv.txt
-
-	# This is just there for the future
-	echo "extraboardargssd=${KERNEL_EXTRA_CMDLINE_SD}" >>"${SDCARD}"/boot/armbianEnv.txt
-	echo "extraboardargsemmc=${KERNEL_EXTRA_CMDLINE_EMMC}" >>"${SDCARD}"/boot/armbianEnv.txt
-}
-
-# almost the last thing before copying to SD.
-# this is called by post_debootstrap_tweaks() after ${SDCARD} is chroot_unmounted
-# -> which is called by  debootstrap_ng() before prepare_partitions()/create_image()
-# -> tmpfs is still going at this point.
-config_post_debootstrap_tweaks__make_sure_hostapd_behaves() {
-	# hostapd REALLY should not be here @TODO: find out really what is installing it and remove this
-	[[ -f "${SDCARD}"/etc/systemd/system/multi-user.target.wants/hostapd.service ]] &&
-		rm -f "${SDCARD}"/etc/systemd/system/multi-user.target.wants/hostapd.service
-
-	[[ -f "${SDCARD}"/lib/systemd/system/hostapd.service ]] &&
-		rm -f "${SDCARD}"/lib/systemd/system/hostapd.service
-
-	[[ -f "${SDCARD}"/etc/init.d/hostapd ]] &&
-		rm -f "${SDCARD}"/etc/init.d/hostapd
-
-	#echo "tree for ${SDCARD}/etc/systemd/ "
-	#tree "${SDCARD}"/etc/systemd
-	#echo "tree for ${SDCARD}/lib/systemd "
-	#tree "${SDCARD}"/lib/systemd
-}
-
-# almost the last thing before copying to SD.
-# this is called by post_debootstrap_tweaks() after ${SDCARD} is chroot_unmounted
-# -> which is called by  debootstrap_ng() before prepare_partitions()/create_image()
-# -> tmpfs is still going at this point.
 config_post_debootstrap_tweaks__restore_systemd_resolved_from_resolvconf_and_armbian() {
 	# do away with the resolv.conf leftover in the image.
 	# set up systemd-resolved which is the way cloud images generally work
@@ -162,4 +129,12 @@ pre_customize_image__restore_preserved_systemd_and_netplan_stuff() {
 
 	# Clean netplan config. Cloud-init will create its own.
 	rm -f "${SDCARD}"/etc/netplan/armbian-default.yaml
+}
+
+pre_umount_final_image__200_add_ci_suffix_to_version() {
+	export version="${version}-cloud"
+	if [[ "a${CLOUD_INIT_USER_DATA_URL}" != "afiles" ]]; then
+		export version="${version}-custom-userdata"
+	fi
+	display_alert "Cloud-init setting version to" "${version}" "wrn"
 }
