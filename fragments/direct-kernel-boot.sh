a596f999a57c88510a0335f1170cecf4ad43d3b8
Ricardo Pardini <ricardo@pardini.net>
2021-05-21 03:32:22 +0200

    x86: little fixes
    Signed-off-by: Ricardo Pardini <ricardo@pardini.net>

--- fragments/direct-kernel-boot.sh
+++ fragments/direct-kernel-boot.sh
@@ -10,7 +10,7 @@ user_config__prepare_dkb() {
 	export COMPRESS_OUTPUTIMAGE=img    # Just the image, please.
 
 	# get rid of packages not really applicable to DKB.
-	remove_packages_everywhere cpufrequtils fake-hwclock haveged rfkill sunxi-tools device-tree-compiler u-boot-tools
+	remove_packages_everywhere cpufrequtils fake-hwclock haveged rfkill sunxi-tools device-tree-compiler u-boot-tools kbd
 
 	# do not write SDCARD for dkb, it makes no sense.
 	export CARD_DEVICE=""
