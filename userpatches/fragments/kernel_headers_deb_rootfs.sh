7c56033b08161b62f74aa7eed4c2ae94bddec725
Ricardo Pardini <ricardo@pardini.net>
2021-05-23 18:29:46 +0200

    fixes/cosmetics
    Signed-off-by: Ricardo Pardini <ricardo@pardini.net>

--- userpatches/fragments/kernel_headers_deb_rootfs.sh
+++ userpatches/fragments/kernel_headers_deb_rootfs.sh
@@ -4,5 +4,6 @@ export COPY_HEADERS_DEB=yes # do copy the headers .deb to rootfs for easy later 
 # Copies a build linux-headers .deb inside the rootfs at /usr/src/
 # Much faster than installing it on rootfs.
 config_post_install_kernel_debs__copy_headers_deb_to_rootfs() {
-	[[ "${COPY_HEADERS_DEB}" == "yes" ]] && cp "${DEB_STORAGE}/${CHOSEN_KERNEL/image/headers}_${REVISION}_${ARCH}.deb" "${SDCARD}"/usr/src/
+	[[ -f "${DEB_STORAGE}/${CHOSEN_KERNEL/image/headers}_${REVISION}_${ARCH}.deb" ]] &&
+		[[ "${COPY_HEADERS_DEB}" == "yes" ]] && cp "${DEB_STORAGE}/${CHOSEN_KERNEL/image/headers}_${REVISION}_${ARCH}.deb" "${SDCARD}"/usr/src/
 }
