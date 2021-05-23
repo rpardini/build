4389e0b83860d46553b08f1361af59a4c1760d6b
Ricardo Pardini <ricardo@pardini.net>
2021-05-23 22:06:01 +0200

    limit-growpart-target-sd: fragment to add marker partition to CARD_DEVICE so growroot does not use ALL the space.
    Signed-off-by: Ricardo Pardini <ricardo@pardini.net>

---
+++ userpatches/fragments/limit-growpart-target-sd.sh
@@ -0,0 +1,13 @@
+# Ok so here we write an extra partition to the SD card, if that was written.
+# This is convenience only, to limit the size to which growpart/growroot will grow the rootfs.
+# It is only done directly on the CARD_DEV device after flashing the img to it.
+
+# Config
+export GROW_LIMIT_GB=16 # adds a partition of the device that wont allow growpart to grow beyond it
+
+# Hooks
+post_write_sdcard__create_growmarker_partition() {
+	display_alert "Creating growmarker partition" "${CARD_DEVICE}" "info"
+	sync # wait for the flashing to finish
+	parted -s "${CARD_DEVICE}" -- mkpart primary ext4 $((GROW_LIMIT_GB * 1024))MiB $(((GROW_LIMIT_GB * 1024) + 1))MiB &>/dev/null || echo "Failed creating growmaker partition on ${CARD_DEVICE}"
+}
