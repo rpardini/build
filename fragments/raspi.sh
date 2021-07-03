7982b84252ff1965f643e4c3fdc1eb1ffc98d6b8
Ricardo Pardini <ricardo@pardini.net>
2021-07-03 17:08:14 +0200

    RaspberryPi4b: rework, avoid using PACKAGE_LIST_BOARD, instead hook post_install_kernel_debs() - uses a hack with /sys/firmware/efi emptydir, that makes flash-kernel skip its run - revert to tty1 mostly so normal people can use, todo implement DEFAULT_CONSOLE

--- fragments/raspi.sh
+++ fragments/raspi.sh
@@ -2,6 +2,9 @@ source "${SRC}/fragments/hack/hack-armbian-packages.sh" # Common hacks with pack
 
 # Config
 export RASPI_ROOT_FS_LABEL="armbian"
+export RASPI_KERNEL_PACKAGES="rpi-eeprom raspi-config linux-firmware-raspi2 linux-tools-raspi linux-raspi linux-image-raspi"
+export RASPI_FLASH_PACKAGES="flash-kernel"
+export RASPI_MACHINE_MODEL="Raspberry Pi 4 Model B"
 
 # Hooks
 user_config__disable_kernel_compile() {
@@ -11,11 +14,30 @@ user_config__disable_kernel_compile() {
 	export UEFI_MOUNT_POINT="/boot/firmware"           # mount uefi part at /boot/firmware
 	export CLOUD_INIT_CONFIG_LOCATION="/boot/firmware" # use /boot/firmware for cloud-init as well
 
-	# Packages for the board.
-	export PACKAGE_LIST_BOARD="${PACKAGE_LIST_BOARD} linux-tools-raspi linux-raspi linux-image-raspi flash-kernel"
+	# Zero out the log for this fragment.
+	echo -n "" >>"${DEST}"/debug/raspi.log
+}
 
-	# Packages to include in rootfs cache
-	export PACKAGE_LIST="${PACKAGE_LIST} rpi-eeprom raspi-config linux-firmware-raspi2"
+# These used to be in PACKAGE_LIST_BOARD, but there is a bit of a conundrum with flash-kernel.
+# A bit of a conundrum with kernel vs flash-kernel.
+# We need to install flash-kernel before the kernel, so that we can then disable/hack it
+# Before actually installing the kernel (which will fail if we don't hack)
+post_install_kernel_debs__install_raspi_ubuntu_kernel_and_flash() {
+	display_alert "Installing raspi kernel" "${RASPI_KERNEL_PACKAGES}"
+	chroot "${SDCARD}" /bin/bash -c "DEBIAN_FRONTEND=noninteractive  apt-get ${apt_extra_dist} -yqq --no-install-recommends install ${RASPI_KERNEL_PACKAGES}" >>"${DEST}"/debug/raspi.log
+
+	display_alert "Installing raspi flash" "${RASPI_FLASH_PACKAGES}"
+	# Create a fake /sys/firmware/efi directory so that flash-kernel does not try to do anything when installed
+	# @TODO: this might or not work after flash-kernel 3.104 or later
+	umount "${SDCARD}"/sys
+	mkdir -p "${SDCARD}"/sys/firmware/efi
+	chroot "${SDCARD}" /bin/bash -c "DEBIAN_FRONTEND=noninteractive  apt-get ${apt_extra_dist} -yqq --no-install-recommends install ${RASPI_FLASH_PACKAGES}" >>"${DEST}"/debug/raspi.log
+
+	# Remove fake /sys/firmware (/efi) directory
+	rm -rf "${SDCARD}"/sys/firmware
+
+	#display_alert "Disabling raspi" "flash-kernel"
+	#chroot "${SDCARD}" /bin/bash -c "chmod -v -x /etc/initramfs/post-update.d/flash-kernel" >>"${DEST}"/debug/raspi.log
 }
 
 # Add a label to the root partition - this is common, should refactor into a separate segment
@@ -43,54 +65,71 @@ pre_update_initramfs__setup_flash_kernel() {
 
 	local update_initramfs_cmd="update-initramfs -c -k all"
 	display_alert "Updating raspi initramfs..." "$update_initramfs_cmd" ""
-	chroot $chroot_target /bin/bash -c "$update_initramfs_cmd" #>>"${DEST}"/debug/install.log 2>&1
+	chroot $chroot_target /bin/bash -c "$update_initramfs_cmd" >>"${DEST}"/debug/raspi.log 2>&1
 
-	local flash_kernel_cmd="flash-kernel --machine 'Raspberry Pi 4 Model B'"
-	display_alert "Raspi flash-kernel" "${flash_kernel_cmd}" "info"
-	chroot $chroot_target /bin/bash -c "${flash_kernel_cmd}" #>>"${DEST}"/debug/install.log 2>&1
+	local flash_kernel_cmd="flash-kernel --machine '${RASPI_MACHINE_MODEL}'"
+	display_alert "Raspi flash-kernel" "${RASPI_MACHINE_MODEL}" "info"
+	chroot $chroot_target /bin/bash -c "${flash_kernel_cmd}" >>"${DEST}"/debug/raspi.log 2>&1
 
 	display_alert "Re-enabling" "initramfs-tools/flash-kernel hook for kernel"
-	chroot $chroot_target /bin/bash -c "chmod -v +x /etc/kernel/postinst.d/initramfs-tools" >>"${DEST}"/debug/install.log 2>&1
-	chroot $chroot_target /bin/bash -c "chmod -v +x /etc/initramfs/post-update.d/flash-kernel" >>"${DEST}"/debug/install.log 2>&1
+	chroot $chroot_target /bin/bash -c "chmod -v +x /etc/kernel/postinst.d/initramfs-tools" >>"${DEST}"/debug/raspi.log 2>&1
+	chroot $chroot_target /bin/bash -c "chmod -v +x /etc/initramfs/post-update.d/flash-kernel" >>"${DEST}"/debug/raspi.log 2>&1
 
 	umount_chroot "$chroot_target/"
 	rm $chroot_target/usr/bin/$QEMU_BINARY
 }
-
-#pre_umount_final_image__show_stuff() {
-#	echo "SHOW STUFF"
-#	tree $MOUNT/boot
-#}
 
 # Internal Functions
 write_raspi_config() {
 	cat <<-EOD >"${MOUNT}"/boot/firmware/config.txt
 		[pi4]
 		max_framebuffers=2
+
 		[all]
 		kernel=vmlinuz
 		cmdline=cmdline.txt
 		initramfs initrd.img followkernel
+
+		# dt params. dunno what are those
 		dtparam=audio=on
 		dtparam=i2c_arm=on
 		dtparam=spi=on
-		enable_uart=1
+
+		# default maybe
 		disable_overscan=1
+
+		# dunno
+		hdmi_drive=2
+
 		arm_64bit=1
-		dtoverlay=dwc2
-		enable_uart=1
+
+		# bootloader logs to serial
+		# enable_uart=1
+		# there is also 'BOOT_UART=1' in 'rpi-eeprom-config' but that is for an earlier stage.
+		# look at with it rpi-eeprom-config
+		# change with 'EDITOR=nano rpi-eeprom-config --edit'
+
+		# overclock. requires decent thermals.
 		over_voltage=6
 		arm_freq=2000
+
+		# disable stuff.
 		dtoverlay=disable-wifi
 		dtoverlay=disable-bt
-		hdmi_drive=2
+
+		# gpu stuff. on 64-bit, its just like panfrost, should work but doesnt, gets better with time
+		# I dont use it at all, but leave enabled for future
 		gpu_mem=256
 		dtoverlay=vc4-fkms-v3d
+
+		# force dwc2, sometimes better for otg stuff and
+		# dtoverlay=dwc2
 	EOD
 }
 write_raspi_cmdline() {
-	# I dont use hdmi: console=tty1
+	# @TODO: consider maybe DEFAULT_CONSOLE, for UART-first console which is great for debugging stuff
+	#        console=serial0,115200
 	cat <<-EOD >"${MOUNT}"/boot/firmware/cmdline.txt
-		dwc_otg.lpm_enable=0 root=LABEL=${RASPI_ROOT_FS_LABEL} rootfstype=ext4 elevator=deadline rootwait fixrtc cgroup_enable=memory cgroup_memory=1 console=serial0,115200
+		root=LABEL=${RASPI_ROOT_FS_LABEL} rootfstype=ext4 elevator=deadline rootwait fixrtc cgroup_enable=memory cgroup_memory=1 console=tty1
 	EOD
 }
